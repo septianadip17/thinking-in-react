5ccb0558ac4a4d7f8ee7a5b0fa927baafb7c5fa6
Septian Adi Pratama <septianadip7998@gmail.com>
2024-08-01 20:28:10 +0700

    new files clean code

---
+++ src/components/FilterableProductTable.js
@@ -0,0 +1,21 @@
+import ProductTable from "./ProductTable";
+
+function SearchBar() {
+  return (
+    <form>
+      <input type="text" placeholder="Search..." />
+      <label>
+        <input type="checkbox" /> Only show products in stock
+      </label>
+    </form>
+  );
+}
+
+export default function FilterableProductTable({ products }) {
+  return (
+    <div>
+      <SearchBar />
+      <ProductTable products={products} />
+    </div>
+  );
+}
